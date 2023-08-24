d6e60ae2a5323efd41da1200896632196a748ce0
Neirea <neireasoultix@gmail.com>
2023-08-25 00:57:36 +0300

    init npm build

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,30 @@
 type Factory<T> = (...args: any[]) => T;
 
-export class ObjPool<T> {
+export default class ObjPool<T> {
     private pool: T[] = [];
-    private size: number = 0;
+    private length: number = 0;
 
     constructor(public factory: Factory<T>) {}
 
     get() {
-        if (this.size > 0) {
-            return this.pool[--this.size];
+        if (this.length > 0) {
+            return this.pool[--this.length];
         }
         return this.factory();
     }
     release(item: T) {
-        this.pool[this.size++] = item;
+        this.pool[this.length++] = item;
     }
     releaseMany(items: T[]) {
         for (let i = 0; i < items.length; i++) {
-            this.pool[this.size++] = items[i];
+            this.pool[this.length++] = items[i];
         }
+    }
+    size() {
+        return this.length;
     }
     clear() {
         this.pool.length = 0;
-        this.size = 0;
+        this.length = 0;
     }
 }
