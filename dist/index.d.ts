150cf6c807bb89d2335093ad2078c2113fcedc72
Neirea <neireasoultix@gmail.com>
2024-03-28 15:05:16 +0200

    preserve JSDoc in builds

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,11 +3,39 @@ export default class ObjPool<T> {
     factory: Factory<T>;
     private pool;
     private length;
+    /**
+     * Creates an object pool.
+     * @param {Factory<T>} factory The factory function to create new objects.
+     */
     constructor(factory: Factory<T>);
+    /**
+     * Retrieves an object from the pool. Creates a new object if the pool is empty.
+     * @returns {T} The retrieved object.
+     */
     get(): T;
+    /**
+     * Releases an object back into the pool.
+     * @param {T} item The object to release.
+     */
     release(item: T): void;
+    /**
+     * Releases multiple objects back into the pool.
+     * @param {T[]} items The objects to release.
+     */
     releaseMany(items: T[]): void;
+    /**
+     * Gets the current size of the pool.
+     * @returns {number} The size of the pool.
+     */
     size(): number;
+    /**
+     * Clears the pool by removing all objects from it.
+     */
     clear(): void;
+    /**
+     * Shrinks the pool to the specified size if it's larger.
+     * @param {number} targetSize The desired size to shrink the pool to. Default is 10.
+     */
+    shrink(targetSize?: number): void;
 }
 export {};
--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,63 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class ObjPool {
+    /**
+     * Creates an object pool.
+     * @param {Factory<T>} factory The factory function to create new objects.
+     */
     constructor(factory) {
         this.factory = factory;
         this.pool = [];
         this.length = 0;
     }
+    /**
+     * Retrieves an object from the pool. Creates a new object if the pool is empty.
+     * @returns {T} The retrieved object.
+     */
     get() {
         if (this.length > 0) {
             return this.pool[--this.length];
         }
         return this.factory();
     }
+    /**
+     * Releases an object back into the pool.
+     * @param {T} item The object to release.
+     */
     release(item) {
         this.pool[this.length++] = item;
     }
+    /**
+     * Releases multiple objects back into the pool.
+     * @param {T[]} items The objects to release.
+     */
     releaseMany(items) {
         for (let i = 0; i < items.length; i++) {
             this.pool[this.length++] = items[i];
         }
     }
+    /**
+     * Gets the current size of the pool.
+     * @returns {number} The size of the pool.
+     */
     size() {
         return this.length;
     }
+    /**
+     * Clears the pool by removing all objects from it.
+     */
     clear() {
         this.pool.length = 0;
         this.length = 0;
+    }
+    /**
+     * Shrinks the pool to the specified size if it's larger.
+     * @param {number} targetSize The desired size to shrink the pool to. Default is 10.
+     */
+    shrink(targetSize = 10) {
+        if (targetSize >= 0 && this.length > targetSize) {
+            this.length = this.pool.length = targetSize;
+        }
     }
 }
 exports.default = ObjPool;
