5a351fdf47a929576c14ff29422b098e5cced3ec
neirea <neirea@ukr.net>
2025-03-21 01:50:27 +0200

    add ignore rule for jest.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
     {
-        ignores: ["eslint.config.js"],
+        ignores: ["eslint.config.js", "jest.config.js"],
     },
     eslint.configs.recommended,
     ...tseslint.configs.recommendedTypeChecked,
